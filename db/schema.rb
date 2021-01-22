9f3f9b6ccdd2b27b3b517b383834d623b2ffb265
sirakan <sirakan@morphos.is>
2021-01-23 03:11:45 +0700

    Fix column name match csv header

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2021_01_22_084340) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.integer "level"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
